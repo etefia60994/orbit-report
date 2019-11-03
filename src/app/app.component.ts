f525cb5c79172fe9273e02fe449757a6c12e9c44
etefia60994 <etefia60994@gmail.com>
2019-11-03 09:24:33 -0600

    Step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Satellite } from './satellite';
-let sourceList: Satellite[];
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,17 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 
 })
-@Satellite({
-  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-    this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ]
-  }
-})
+
 export class AppComponent {
   title = 'orbit-report';
+  sourceList : Satellite[];
+  constructor(){
+  this.sourceList = [
+    new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+    new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+    new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+    new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+    new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+  ]
+}
 }
